663563679ae0503cf44e77348f12143e61496be7
happilyleng <77030312+happilyleng@users.noreply.github.com>
2025-03-07 03:16:06 +0800

    Add files via upload

---
+++ mizu/mizu Watch App/mizuApp.swift
@@ -0,0 +1,17 @@
+//
+//  mizuApp.swift
+//  mizu Watch App
+//
+//  Created by mac on 2025/1/23.
+//
+
+import SwiftUI
+
+@main
+struct mizu_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mizu/mizu Watch AppTests/mizu_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  mizu_Watch_AppTests.swift
+//  mizu Watch AppTests
+//
+//  Created by mac on 2025/1/23.
+//
+
+import Testing
+@testable import mizu_Watch_App
+
+struct mizu_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
